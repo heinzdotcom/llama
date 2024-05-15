bf6800ade9fa4a8436cd19afb0ec029bf7f54aab
heinzdotcom <71572792+heinzdotcom@users.noreply.github.com>
2024-05-15 14:06:11 +0000

    new file

--- example_chat_completion.py
+++ example_chat_completion.py
@@ -1,4 +1,4 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
+example()_chat# Copyright (c) Meta Platforms, Inc. and affiliates.
 # This software may be used and distributed according to the terms of the Llama 2 Community License Agreement.
 
 from typing import List, Optional
