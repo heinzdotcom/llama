db4382b1234be984444f29516339a10bc8fc9b44
heinzdotcom <71572792+heinzdotcom@users.noreply.github.com>
2024-05-17 20:53:14 +0000

    new file

--- main.py
+++ main.py
@@ -1,2 +1,2 @@
-def main_example_chat_competion.py_object()create tokenization_path:ckpt_dir (str),
+def main_example_chat_competion.py_object()create tokenization_path:ckpt_dir.("workspaces.TextTestResult(stream, descriptions, verbosity)"),
     ckpt_dir:(main.py),
